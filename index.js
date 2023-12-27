5200e49ea27960debf0b64ab466951e579fbe5f3
Abiolayoung <abiolayoung229@gmail.com>
2023-12-27 11:37:12 +0100

    added content

---
+++ index.js
@@ -0,0 +1,35 @@
+// switch statement- this is a statement that can replace multiple if checks
+// switch(x){
+//     case 'value1':
+//         ...
+//        [break]
+
+//     case 'value2':
+//         ...
+//         [break]
+
+//     default: 
+//         ...
+//         [break]
+// }
+
+let a = 2+2;
+switch (a){
+    case 4:
+        alert('you are right')
+        break;
+
+case 3:
+    alert('you are short')
+    break;
+
+    default:
+        alert(unmatched)
+        break;
+}
+
+// break can be used to break out of each loop after execution
+
+// making shapes with loop
+
+// 1. creating a square shape
