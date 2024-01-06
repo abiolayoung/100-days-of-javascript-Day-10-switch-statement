decfddfcfeac992f5ed65e9022383330425b2679
Abiolayoung <abiolayoung229@gmail.com>
2024-01-07 00:32:20 +0100

    updates

--- index.js
+++ index.js
@@ -79,20 +79,31 @@
 
 // creating a hollow right angle triangle
 
+// let n = 5;
+// let string = "";
+// for(let i=1; i<=n; i++){
+//     for(let j=0; j<i; j++){
+//     if(i===n){
+//         string += "*";
+//     }else{
+//         if (j==0 || j==i-1){
+//             string += "*";
+//         }else {
+//             string += " ";
+//         }
+//     }
+//     }
+//     string+="\n"
+// }
+// alert(string)
+
 let n = 5;
 let string = "";
-for(let i=1; i<=n; i++){
-    for(let j=0; j<i; j++){
-    if(i===n){
-        string += "*";
-    }else{
-        if (j==0 || j==i-1){
-            string += "*";
-        }else {
-            string += " ";
-        }
+
+for(let i = 0; i<n; i++){
+    for(let j=0; j<n; j++){
+        string+= "*"
     }
-    }
-    string+="\n"
+    string +='\n';
 }
 alert(string)
